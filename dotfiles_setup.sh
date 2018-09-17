43e29e59bdf604a7284b48f1fa4b68d09116e218
byjones <byjones@mymail.mines.edu>
2018-09-16 19:14:29 -0600

    separated ssh config setup from dotfile setup script

--- dotfiles_setup.sh
+++ dotfiles_setup.sh
@@ -7,6 +7,3 @@ dir="${HOME}/dev/dotfiles"
 for dotfile in "${dotfiles[@]}";do
 	ln -sf "${dir}/${dotfile}" "${HOME}" 
 done
-
-cat "${dir}/config" > "${HOME}/.ssh/config"
-chmod 644 "${HOME}/.ssh/config"
