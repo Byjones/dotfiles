a8ab893008c12d490722fa9a92e187fda6b7f8d7
byjones <byjones@mymail.mines.edu>
2018-09-19 12:41:44 -0600

    added directory parameter for dotfile setup script

--- bin/dotfiles_setup.sh
+++ bin/dotfiles_setup.sh
@@ -2,7 +2,7 @@
 
 dotfiles=(".vimrc" ".bash_aliases" ".gitconfig" ".tmux.conf" "bin/")
 
-dir="${HOME}/dev/dotfiles"
+dir="${HOME}/"$1"/dotfiles"
 
 for dotfile in "${dotfiles[@]}";do
 	ln -sf "${dir}/${dotfile}" "${HOME}" 
